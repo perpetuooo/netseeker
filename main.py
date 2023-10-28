6bdfd8cdd1fec6d5df5fc02abf1703782dff618a
Pedro <pedrohenriqueperpetuo@gmail.com>
2023-10-28 12:47:58 -0300

    host discovery

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def hello_world():
 def threaded_port_scanner(ip: Annotated[str, typer.Argument(help="Target IP/domain.")] = '127.0.0.1',
                           ports: Annotated[str, typer.Argument(help="Desired port range to scan.")] = '1-1024', 
                            threads: Annotated[int, typer.Option(help="Threads number for the scanner.")] = 20):
-    """Scan the given ports of the given IP address."""
-    ip_pattern = r'(\d+)[-,.;](\d+)'
-    match = re.search(ip_pattern, ports)
+    """Scan the given ports of the target address."""
+    ports_pattern = r'(\d+)[-,.;](\d+)'
+    match = re.search(ports_pattern, ports)
 
     if match:
         start = int(match.group(1))
@@ -31,6 +31,13 @@ def threaded_port_scanner(ip: Annotated[str, typer.Argument(help="Target IP/doma
 
     else:
         raise ValueError("[!] Invalid port range, use 'start-end' or 'start,end'.")
+
+
+@app.command("wifi-scanner")
+def host_discovery(ip: Annotated[str, typer.Argument(help="Target IP.")] = '192.168.1.0/24',
+                 verbose: Annotated[bool, typer.Option(help="Verbose flag.")] = False):
+    """Discover all devices on the local network."""
+    network_scanner.ScapyNetScanner(ip, verbose)
 
 
 
