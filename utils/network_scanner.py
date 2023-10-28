625838cd0502859659bfdc2bcf3fc64c543a48e3
Pedro <pedrohenriqueperpetuo@gmail.com>
2023-10-28 13:13:52 -0300

    scanners update (improvement needed)

--- utils/network_scanner.py
+++ utils/network_scanner.py
@@ -1,31 +1,52 @@
+import sys
+import socket
 import scapy.all as scapy
+from rich import print
+from rich.table import Table
 
-def ScapyNetScanner(ip):
-    result = []
+def ScapyNetScanner(target, vFlag):
+    print(f"[bold yellow][-] Scanning devices on {target}...[/bold yellow]\n")
 
-    def scanner(target):
+    table = Table("IP", "MAC", "HOSTNAME")
+
+    def scanner(ip):
         try:
-            arp_request = scapy.ARP(pdst=target)
+            arp_request = scapy.ARP(pdst=ip)
             #arp_request.show()
             ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
             #ether_frame.show()
             request = ether_frame/arp_request
             #request.show()
 
-            response_list = scapy.srp(request, timeout=3, retry=1, verbose=True)[0]
+            response_list = scapy.srp(request, timeout=3, retry=1, verbose=vFlag)[0]
 
             for sent, received in response_list:
                 #print(received.show())
-                result.append({'IP': received.psrc, 'MAC': received.hwsrc})
+                try:
+                    hostname = socket.gethostbyaddr(received.psrc)
+                    #print(hostname)
+
+                except:
+                    hostname = ("???",)
+
+                table.add_row(received.psrc, received.hwsrc, str(hostname[0]))
 
         except Exception as e:
-            print(str(e))
+            print(f"[bold red][!] ERROR: {str(e)}[/bold red]")
+            sys.exit(1)
+        
+        except KeyboardInterrupt:
+            sys.exit()
 
 
-    scanner(ip)
+    scanner(target)
+
+    if table.row_count == 0:
+        print("[bold red][-] Scanner Failed.[/bold red]")
     
-    for device in result:
-        print(device['IP'] + "\t" + device['MAC'])
+    else:
+        print(table)
+
 
 if __name__ == '__main__':
     pass
--- utils/port_scanner.py
+++ utils/port_scanner.py
@@ -1,10 +1,11 @@
+import sys
 import nmap
 from rich import print
 from rich.table import Table
 from concurrent.futures import ThreadPoolExecutor
 
-def NmapScanner(target, start, end, threads):
-    print(f"[bold yellow][%] Scanning ports {start} to {end} on {target}...[/bold yellow]")
+def NmapPortScanner(target, start, end, threads):
+    print(f"[bold yellow][-] Scanning ports {start} to {end} on {target}...[/bold yellow]\n")
 
     nm = nmap.PortScanner()
     table = Table("Port", "State", "Service")
@@ -21,14 +22,18 @@ def NmapScanner(target, start, end, threads):
                 table.add_row(str(port), port_state, port_service)
 
         except Exception as e:
-            print(f"[bold red][!] ERROR: {e}[/bold red]")
+            print(f"[bold red][!] ERROR: {str(e)}[/bold red]")
+            sys.exit(1)
+        
+        except KeyboardInterrupt:
+            sys.exit()
 
 
     with ThreadPoolExecutor(max_workers=threads) as executor:
         executor.map(scanner, range(start, end + 1))
 
     if table.row_count == 0:
-        print(f"\n[bold red][!] No open ports on {target}.[/bold red]")
+        print(f"[bold red][!] No open ports on {target}.[/bold red]")
 
     else:
         print(table)
