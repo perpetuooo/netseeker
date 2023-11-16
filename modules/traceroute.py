6537037c0070ad237bb60527070653d8ed85aaeb
Pedro <pedrohenriqueperpetuo@gmail.com>
2023-11-16 14:36:50 -0300

    Update traceroute.py

--- modules/traceroute.py
+++ modules/traceroute.py
@@ -1,33 +1,37 @@
 import subprocess
 import requests
 import platform
+import socket
 import sys
 import re
 from rich import print
 from datetime import datetime
+from alive_progress import alive_bar
 
 """
 ----  TO DO:  ----
-finish sockets prep;
-while loop;
-create a map with the info provided (if possible);
+find a way to organize the the result output; 
+create a map with the info provided;
 """
 
 def Traceroute(target):
 
+    #getting location info from the ip address
     def get_location(ip):
+        if ip.endswith(".com"):
+            ip = socket.gethostbyname(ip)
+
         response = requests.get(f'https://ipapi.co/{ip}/json/').json()
         location_data = {
         "city": response.get("city"),
         "country": response.get("country_name"),
         "lat": response.get("latitude"),
         "long": response.get("longitude")}
-    
+
         return location_data
 
 
-    print(get_location(target))
-
+    #changing the command depending on the OS
     if platform.system() == "Windows":
         command = "tracert"
 
@@ -38,8 +42,32 @@ def Traceroute(target):
         print(f"[bold red][!] Invalid OS.[/bold red]")
         sys.exit(1)
 
-    result = subprocess.run(command + target)
-    print(result)
+
+    with alive_bar(title="Tracerouting...", bar=None, spinner="classic", monitor=False, elapsed=False, stats=False) as bar:
+        result = subprocess.run([command, target], stdout=subprocess.PIPE, text=True, universal_newlines=True)
+        bar.title("Done!")
+
+    for line in result.stdout:
+        print(line, end="")
+
+    filtred_result = result.stdout.splitlines()
+    ip_pattern = r'(\d+\.\d+\.\d+\.\d+)'
+    ip_list = []
+
+    for line in filtred_result:
+        match = re.findall(ip_pattern, line)
+
+        if match:
+            ip_list.extend(match)
+
+    print('\n')
+
+    for ip in ip_list:
+        try:
+            print(f"{ip} : {get_location(ip)}")
+
+        except:
+            print(f"{ip} location not found...")
 
 
 
