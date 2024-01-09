b49ffa81d497ae8b9bd9be19e09cb37546621b4a
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-01-09 20:19:08 -0300

    Update netseeker.py

--- netseeker.py
+++ netseeker.py
@@ -1,5 +1,5 @@
 import sys
-import typer
+from typer import Typer, Argument, Option
 from typing_extensions import Annotated
 
 from modules import traceroute
@@ -7,38 +7,38 @@ from modules import port_scanner
 from modules import network_scanner
 from modules import arp_spoofer
 
-app = typer.Typer(rich_markup_mode="rich", hidden=True)
+app = Typer(rich_markup_mode="rich")
 
 
 @app.command("traceroute")
-def tracert(target: Annotated[str, typer.Argument(help="Target IP/domain.", show_default=False)],
-             timeout: Annotated[int, typer.Option(help="Timeout for receiving packets.")] = 5):
+def tracert(target: Annotated[str, Argument(help="Target IP/domain.", show_default=False)],
+             timeout: Annotated[int, Option(help="Timeout for receiving packets.")] = 5):
     """Trace the path of IP packets with its location."""
     traceroute.TracerouteWithMap(target, timeout)
 
 
 @app.command("port-scanner")
-def threaded_port_scanner(ip: Annotated[str, typer.Argument(help="Target IP/domain (loopback for default).", show_default=False)] = '127.0.0.1',
-                           ports: Annotated[str, typer.Option(help="Desired port range to scan (ex: 1-1024).", show_default=False)] = '1-1024', 
-                            threads: Annotated[int, typer.Option(help="Threads amount for the scanner process.")] = 20,
-                             args: Annotated[str, typer.Option(help="Other arguments for the scanner", show_default=False)] = ''):
+def threaded_port_scanner(ip: Annotated[str, Argument(help="Target IP/domain (loopback for default).", show_default=False)] = '127.0.0.1',
+                           ports: Annotated[str, Option(help="Desired port range to scan (ex: 1-1024).", show_default=False)] = '1-1024', 
+                            threads: Annotated[int, Option(help="Threads amount for the scanner process.")] = 20,
+                             args: Annotated[str, Option(help="Other arguments for the scanner", show_default=False)] = ''):
     """Scan the given ports of the target address."""
     port_scanner.NmapPortScanner(ip, ports, threads, args)
 
 
 @app.command("host-discovery")
-def host_discovery(ip: Annotated[str, typer.Argument(help="Target IP range (ex: 192.168.1.1/24).", show_default=False)],
-                    args: Annotated[str, typer.Option(help="Other arguments for the scanner.")] = "-sn",
-                     timing: Annotated[int, typer.Option(help="[b]0[/b] (slower scans but harder to be detected) to [b]5[/b] (faster scans but very agressive).")] = 3):
+def host_discovery(ip: Annotated[str, Argument(help="Target IP range (ex: 192.168.1.1/24).", show_default=False)],
+                    args: Annotated[str, Option(help="Other arguments for the scanner.")] = "-sn",
+                     timing: Annotated[int, Option(help="[b]0[/b] (slower scans but harder to be detected) to [b]5[/b] (faster scans but very agressive).")] = 3):
     """Discover all devices on the local network."""
     network_scanner.NmapNetScanner(ip, timing, args)
 
 
 @app.command("arp-spoofer")
-def arp_poisoning(target: Annotated[str, typer.Argument(help="Target IP.", show_default=False)],
-                   host: Annotated[str, typer.Argument(help="Target host (default gateway for default).", show_default=False)] = "",
-                    timing: Annotated[int, typer.Option(help="Timing between sending packets.", show_default=False)] = 2,
-                     verbose: Annotated[bool, typer.Option(help="Verbose flag.", show_default=False)] = 'False'):
+def arp_poisoning(target: Annotated[str, Argument(help="Target IP.", show_default=False)],
+                   host: Annotated[str, Argument(help="Target host (default gateway for default).", show_default=False)] = "",
+                    timing: Annotated[int, Option(help="Timing between sending packets.", show_default=False)] = 2,
+                     verbose: Annotated[bool, Option(help="Verbose flag.", show_default=False)] = 'False'):
     """Not working yet."""
     arp_spoofer.ScapyArpSpoofer(target, host, timing, verbose)
 
