418aa54ecdb1c885c7cde2f4122e47463eb9860f
Pedro <pedrohenriqueperpetuo@gmail.com>
2024-01-09 19:30:23 -0300

    giving up on argparse

--- netseeker.py
+++ netseeker.py
@@ -1,21 +1,19 @@
-import os
 import sys
-import rich
-from argparse import ArgumentParser, Namespace
+import typer
+from typing_extensions import Annotated
 
 from modules import traceroute
 from modules import port_scanner
 from modules import network_scanner
 from modules import arp_spoofer
 
-"""
-app = typer.Typer(rich_markup_mode="rich")
+app = typer.Typer(rich_markup_mode="rich", hidden=True)
 
 
 @app.command("traceroute")
 def tracert(target: Annotated[str, typer.Argument(help="Target IP/domain.", show_default=False)],
              timeout: Annotated[int, typer.Option(help="Timeout for receiving packets.")] = 5):
-    ""Trace the path of IP packets with its location.""
+    """Trace the path of IP packets with its location."""
     traceroute.TracerouteWithMap(target, timeout)
 
 
@@ -24,7 +22,7 @@ def threaded_port_scanner(ip: Annotated[str, typer.Argument(help="Target IP/doma
                            ports: Annotated[str, typer.Option(help="Desired port range to scan (ex: 1-1024).", show_default=False)] = '1-1024', 
                             threads: Annotated[int, typer.Option(help="Threads amount for the scanner process.")] = 20,
                              args: Annotated[str, typer.Option(help="Other arguments for the scanner", show_default=False)] = ''):
-    ""Scan the given ports of the target address.""
+    """Scan the given ports of the target address."""
     port_scanner.NmapPortScanner(ip, ports, threads, args)
 
 
@@ -32,7 +30,7 @@ def threaded_port_scanner(ip: Annotated[str, typer.Argument(help="Target IP/doma
 def host_discovery(ip: Annotated[str, typer.Argument(help="Target IP range (ex: 192.168.1.1/24).", show_default=False)],
                     args: Annotated[str, typer.Option(help="Other arguments for the scanner.")] = "-sn",
                      timing: Annotated[int, typer.Option(help="[b]0[/b] (slower scans but harder to be detected) to [b]5[/b] (faster scans but very agressive).")] = 3):
-    ""Discover all devices on the local network.""
+    """Discover all devices on the local network."""
     network_scanner.NmapNetScanner(ip, timing, args)
 
 
@@ -41,42 +39,24 @@ def arp_poisoning(target: Annotated[str, typer.Argument(help="Target IP.", show_
                    host: Annotated[str, typer.Argument(help="Target host (default gateway for default).", show_default=False)] = "",
                     timing: Annotated[int, typer.Option(help="Timing between sending packets.", show_default=False)] = 2,
                      verbose: Annotated[bool, typer.Option(help="Verbose flag.", show_default=False)] = 'False'):
-    ""Not working yet.""
+    """Not working yet."""
     arp_spoofer.ScapyArpSpoofer(target, host, timing, verbose)
-"""
+
 
 
 def main():
-
-    """
-    ----  TO DO:  ----
-    using the "-h" argument breaks the loop, find out why;
-    """
-
-    parser = ArgumentParser()
-    parser.add_argument('echo', help="Echos the given string.")
-    parser.add_argument('-c', '--caps', help="Capitalizes the echo strig.", action='store_true')
-
     while True:
         try:
-            command = input(f"\necho input > ")
+            command = input(f"\ntesting > ")
+            app(command.split())
 
-            args: Namespace = parser.parse_args(command.split())
-
-            if args.caps and args.echo:
-                print((args.echo).upper())
-
-            elif args.echo:
-                print(args.echo)
-
-            else:
-                print("Invalid argument, try again...")
-
-        #except Exception:
-            #print("Invalid command, try again...")
+        except SystemExit:
+            if SystemExit.code == 0:
+                continue
 
         except KeyboardInterrupt:
             sys.exit(0)
+
 
 
 if __name__ == "__main__":
