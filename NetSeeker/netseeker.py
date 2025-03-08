d480af9601d51a2a59432b35a728e18cb290a28d
PH Perpétuo <pedrohenriqueperpetuo@gmail.com>
2025-03-08 00:29:10 -0300

    better commands

--- NetSeeker/netseeker.py
+++ NetSeeker/netseeker.py
@@ -1,10 +1,11 @@
 from typer import Typer, Argument, Option
 from typing_extensions import Annotated
+from rich.console import Console
 
-from modules import traceroute
 from modules import port_scanner
-from modules import network_scanner
-from modules import arp_spoofer
+# from modules import traceroute
+# from modules import network_scanner
+# from modules import arp_spoofer
 
 app = Typer(rich_markup_mode="rich")
 
@@ -20,31 +21,31 @@ def tracert(target: Annotated[str, Argument(help="Target IP/domain.")] = "",
             timeout: Annotated[int, Option("--timeout", "-t", help="Timeout for receiving packets (milliseconds).")] = 5000,
             result_map: Annotated[bool, Option("--map", "-m", help="Create a map with the info provided.")] = False):
     """Trace the path of IP packets."""
-    traceroute.TracerouteWithMap(target, timeout, result_map)
+    # traceroute.TracerouteWithMap(target, timeout, result_map)
 
 
-@app.command("port-scanner")
-def threaded_port_scanner(ip: Annotated[str, Argument(help="Target IP/domain (loopback for default).")] = "",
-                          ports: Annotated[str, Argument(help="Desired port range to scan (example: 1-1024).")] = "", 
+@app.command("portscan")
+def threaded_port_scanner(ip: Annotated[str, Argument(help="Target IP/domain.")] = "127.0.0.1",
+                          ports: Annotated[str, Argument(help="Desired ports to scan (ex: 80 / 1-1024 / 20,22,443).")] = "1-1024", 
                           threads: Annotated[int, Option("--threads", "-t", help="Threads amount for the scanner process.")] = 20):
     """Scan the given ports of the target address."""
-    port_scanner.NmapPortScanner(ip, ports, threads)
+    port_scanner.portScanner(ip, ports, threads)
 
 
-@app.command("host-discovery")
+@app.command("netscan")
 def host_discovery(ip: Annotated[str, Argument(help="Target IP range (ex: 192.168.1.1/24).")] = "",
-                   timing: Annotated[int, Option("--timing", "-t", help="[b]0[/b] (slower scans but harder to be detected) to [b]5[/b] (faster scans but very agressive).")] = 3):
+                   timing: Annotated[int, Option("--timing", "-t", help="[b]0[/b] (slower scans but harder to be detected) to [b]5[/b] (faster but agressive scans).")] = 3):
     """Discover all devices on the local network."""
-    network_scanner.NmapNetScanner(ip, timing)
+    # network_scanner.NmapNetScanner(ip, timing)
 
 
-@app.command("arp-spoofer")
+@app.command("arpspoofer")
 def arp_poisoning(target: Annotated[str, Argument(help="Target IP.")] = "",
                   host: Annotated[str, Argument(help="Target host.")] = "",
                   timing: Annotated[int, Option("--timing", "-t", help="Timing between sending packets.")] = 2,
                   verbose: Annotated[bool, Option("--verbose", "-v", help="Verbose flag.")] = 'False'):
-    """Not working yet."""
-    arp_spoofer.ScapyArpSpoofer(target, host, timing, verbose)
+    """Not working (yet)."""
+    # arp_spoofer.ScapyArpSpoofer(target, host, timing, verbose)
 
 
 
