581576d345ae842c9cd3f2e71766c18021b3a756
PH Perpétuo <pedrohenriqueperpetuo@gmail.com>
2025-03-09 20:02:07 -0300

    get user network function

--- NetSeeker/resources/services.py
+++ NetSeeker/resources/services.py
@@ -1,8 +1,9 @@
 import ipaddress
 import scapy.all as scapy
+import netifaces
 
 class DevicesInfo:
-    # Sends an ICMP packet to the target host, waiting for a reply.
+    # Sends an ICMP echo request to the target host and checks for a reply.
     def ping(self, target):
         packet = scapy.IP(dst=target) / scapy.ICMP()
         reply = scapy.sr1(packet, timeout=1, verbose=False)
@@ -10,7 +11,7 @@ class DevicesInfo:
         return True if reply else False
 
 
-    # Gets the MAC address from the target IP using an ARP request.
+    # Retrieves the MAC address associated with the target IP using an ARP request.
     def get_mac(self, ip):
         arp_request = scapy.ARP(pdst=ip)
         ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -25,14 +26,27 @@ class DevicesInfo:
             raise Exception(f"{ip} MAC not found.")
 
 
-    # Gets default gateway.
+    # Retrieves the default gateway IP address for the current network.
     def get_default_gateway(self):
         gw = scapy.conf.route.route("0.0.0.0")[2]
 
         return gw
 
 
-    # Checks IPv4 type.
+    # Calculates and retrieves the local network in CIDR notation.
+    def get_network(self):
+        gateways = netifaces.gateways()
+        default_gateway = gateways['default'][netifaces.AF_INET]
+        interface = default_gateway[1]
+
+        addr = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]
+        ip = addr['addr']
+        mask = addr['netmask']
+
+        return ipaddress.IPv4Network(f"{ip}/{mask}", strict=False)
+
+
+    # Checks if the given IP address is a valid public IPv4 address.
     def check_ipv4(self, ip):
         try:
             target = ipaddress.ip_address(ip)
