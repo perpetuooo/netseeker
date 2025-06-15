219495854f0b6c8ce82e9320bd7b2be48691a264
PH Perpétuo <pedrohenriqueperpetuo@gmail.com>
2025-06-15 00:23:10 -0300

    refactor: sdenum - improving logic

--- NetSeeker/modules/sd_enum.py
+++ NetSeeker/modules/sd_enum.py
@@ -1,6 +1,8 @@
+import re
 import time
 import typer
 import socket
+import requests
 import random
 import string
 from dns import resolver
@@ -35,6 +37,7 @@ def subdomainEnumeration(target, wordlist_path, timeout, ipv6, output, http_stat
             progress.console.print(f"[bold red][!] ERROR:[/bold red] Invalid file path for wordlist: {filepath}") 
             raise typer.Exit(code=1)
 
+
     # Detect wildcard addresses to ignore any resolutions on the scanner function.
     def detect_wildcard(domain):
 
@@ -43,89 +46,93 @@ def subdomainEnumeration(target, wordlist_path, timeout, ipv6, output, http_stat
             random.seed(time.time())
             return ''.join(random.choice((string.ascii_letters + string.digits)) for _ in range(12))
 
-        detected_a_records = []
-        detected_cnames = []
-        wildcard_ips = set()
-        wildcard_cnames = set()
+        detected_a = []
+        detected_aaaa = []
+        wildcard_ips = []
 
         for _ in range(3):
             sub = f"{generate_subdomain()}.{domain}"
 
-            # A record check.
+            # A records check.
             try:
                 response = resolver.resolve(sub, 'A')
                 ips = sorted(ip.to_text() for ip in response)
-                detected_a_records.append(tuple(ips))
-            except Exception:
-                detected_a_records.append(None)
+                detected_a.append(tuple(ips))
+            except: pass
 
-            # CNAME check.
-            try:
-                cname_response = resolver.resolve(sub, 'CNAME')
-                cnames = sorted(c.to_text() for c in cname_response)
-                detected_cnames.append(tuple(cnames))
-            except:
-                detected_cnames.append(None)
+            # AAAA records check.
+            if ipv6:
+                try:
+                    response6 = resolver.resolve(sub, 'AAAA')
+                    ips6 = sorted(c.to_text() for c in response6)
+                    detected_aaaa.append(tuple(ips6))
+                except: pass
 
-        success_a = [ip for ip in detected_a_records if ip]
-        success_c = [c for c in detected_cnames if c]
-
-        if not success_a and not success_c:
+        if not detected_a and not detected_aaaa:
             progress.console.print(f"[bold yellow]\\[i][/bold yellow] No wildcard DNS detected.")
             return None 
 
-        # Handle A record wildcard detection.
-        unique_a = set(success_a)
-        if len(unique_a) == 1:
-            wildcard_ips = set(unique_a.pop())
-            progress.console.print(f"[bold yellow]\\[i][/bold yellow] A-record widlcard DNS detected: [yellow]{''.join(wildcard_ips)}[/yellow]")
-        else:
-            progress.console.print(f"[bold yellow]\\[i][/bold yellow] Inconsistent A-record wildcard behavior detected, ignoring results[white]...")
+        if detected_a and len(set(detected_a)) == 1:
+            progress.console.print(f"[bold yellow]\\[i][/bold yellow] A-record wildcard DNS detected: {"".join(ips)}")
+            wildcard_ips.extend(detected_a[0])
 
-        # Handle CNAME wildcard detection.
-        unique_cnames = set(success_c)
-        if len(unique_cnames) == 1:
-            wildcard_cnames = set(unique_cnames.pop())
-            progress.console.print(f"[bold yellow]\\[i][/bold yellow] CNAME wildcard DNS detected: [yellow]{wildcard_cnames}[/yellow]")
-        else:
-            progress.console.print(f"[bold yellow]\\[i][/bold yellow] Inconsistent CNAME wildcard behavior detected, ignoring results[white]...")
+        if detected_aaaa and len(set(detected_aaaa)) == 1:
+            progress.console.print(f"[bold yellow]\\[i][/bold yellow] AAAA-record wildcard DNS detected: {"".join(ips6)}")
+            wildcard_ips.extend(detected_aaaa[0])
 
-        return wildcard_ips.union(wildcard_cnames)
+        return wildcard_ips
+
 
     #
     def scanner(subdomain):
 
-        # def check_http():
-        #   pass
+        #
+        def check_http():
+          pass
 
-        # def bruteforce():
-        #   pass
 
-        try:
-            full_domain = f"{subdomain}.{target}"
-            result = resolver.resolve(full_domain)
-            ips = [ip.to_text() for ip in result]
+        with progress_lock:
+            progress.update(task_id, description=f"Scanning [yellow]{target}[/yellow] for subdomains - {subdomain}")
 
-            # Skip if it's a wildcard response.
-            if any(ip in wildcard_ips for ip in ips):
-                return
+        record_types = ["A", "CNAME", "TXT", "NS"]
+        if ipv6: record_types.append("AAAA")
 
-            progress.console.print(f"[bold green][+][/bold green] Found subdomain: [green][link=https://{full_domain}]{full_domain}[/link][/green]")
+        full_domain = f"{subdomain}.{target}"
+        found = False
 
-            with progress_lock:
-                found_subdomains.append(full_domain)
+        # Searching subdomains by bruteforce.
+        for rtype in record_types:
+            try:
+                result = resolver.resolve(full_domain, rtype)
+                ips = [ip.to_text() for ip in result]
 
-        except Exception:
-            pass
+                # Wildcard filtering for A/AAAA.
+                if rtype in ("A", "AAAA") and any(ip in wildcard_ips for ip in ips):
+                    return
 
-        finally:
-            progress.update(task_id, advance=1)
+                if not found:
+                    progress.console.print(f"[bold green][+][/bold green] Found subdomain: [green]{full_domain}[/green]")
+
+                    if http_status: check_http()
+
+                    with progress_lock:
+                        found_subdomains.append(full_domain)
+
+                    found = True
+                    break
+
+            # Domain name not found or time expired.
+            except (resolver.NXDOMAIN, resolver.Timeout, resolver.NoAnswer):
+                pass
+
+            finally:
+                progress.update(task_id, advance=1)
 
 
     info = services.DevicesInfo()
     stop = Event()
     progress_lock = Lock()
-    subdomains = set()
+    subdomains = []
     found_subdomains = []
 
     if not info.check_domain(target):
@@ -143,15 +150,15 @@ def subdomainEnumeration(target, wordlist_path, timeout, ipv6, output, http_stat
             task_id:TaskID = progress.add_task("Starting enumeration...")
             
             # Loading subdomains.
-            progress.update(task_id, description=f"Loading wordlist")
-            load_wordlist('resources/txt/subdomains.txt', subdomains)   # need to create your own subdomains.txt (using subbrute list)
-            if wordlist_path: load_wordlist(wordlist_path, subdomains)
+            progress.update(task_id, description=f"Loading wordlist...")
+            if wordlist_path: 
+                load_wordlist(wordlist_path, subdomains)
+                progress.console.print("[bold green][+][/bold green] Loaded wordlist successfully!")
+            load_wordlist('resources/txt/subdomains.txt', subdomains)   # need to create my own subdomains.txt (using subbrute list)
 
             # Detecting wildcard DNS.
-            progress.update(task_id, description=f"Detecting wildcard addresses")
+            progress.update(task_id, description=f"Detecting wildcard addresses...")
             wildcard_ips = detect_wildcard(target)
-
-            progress.update(task_id, description=f"Scanning [yellow]{target}[/yellow] for subdomains (this might take a while)", total=len(subdomains))
 
             with ThreadPoolExecutor(max_workers=threads) as executor:    # Using ThreadPoolExecutor to improve performance.
                 futures = {executor.submit(scanner, subdomain): subdomain for subdomain in subdomains}
