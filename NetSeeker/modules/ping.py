23f7d35efe233bba4cdcc42f6a2052fe48b38be4
PH Perpétuo <pedrohenriqueperpetuo@gmail.com>
2025-07-31 19:09:47 -0300

    refactor: ping - improve TTL handling and restructure ping logic

--- NetSeeker/modules/ping.py
+++ NetSeeker/modules/ping.py
@@ -1,14 +1,15 @@
 import os
+import sys
 import time
 import socket
 import struct
-import platform
+from rich.progress import Progress, SpinnerColumn, TextColumn, TaskID
 
 from resources import console
 
 def packetInternetGrouper(target, timeout, count, ttl):
     # Sets the socket TTL based on the users plataform.
-    def set_ttl(ttl):
+    def set_ttl(sock, ttl):
         success = False
 
         # Attempt with SOL_IP
@@ -31,7 +32,8 @@ def packetInternetGrouper(target, timeout, count, ttl):
                 pass
         
         if not success:
-            console.print(f"[bold red][!][/bold red] Failed to set TTL to {ttl}.")
+            console.print(f"[bold red][!][/bold red] Failed to set TTL to {ttl}, using default values.")
+            ttl = 0     # To stop trying to set a new TTL value every iteration.
 
 
     # Perform internet checksum.
@@ -59,40 +61,59 @@ def packetInternetGrouper(target, timeout, count, ttl):
 
         return answer
 
+    
+    def ping():
+        nonlocal received, miss
+
+        for n in range(count):
+            try:
+                # Create ICMP raw socket.
+                sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+                sock.settimeout(timeout)
+                set_ttl(sock, ttl)
+                packet_id = os.getpid() & 0xFFFF
+                header = struct.pack("bbHHh", 8, 0, 0, packet_id, n)
+                data = struct.pack("d", time.time())
+
+                checksum_val = checksum(header + data)
+                header = struct.pack("bbHHh", 8, 0, socket.htons(checksum_val), packet_id, n)
+                packet = header + data
+
+                sock.sendto(packet, (target, 1))
+                start = time.time()
+                reply, addr = sock.recvfrom(1024)
+                duration = (time.time() - start) * 1000
+                responses.append(duration)
+
+                if reply:
+                    console.print(f"[bold green][+][/bold green] Response received from {addr[0]} - {duration:.2f}ms")
+                    received += 1
+            except socket.timeout:
+                console.print("[bold yellow][~][/bold yellow] Request timed out.")        
+                miss += 1
+            finally:
+                sock.close()
+ 
+
+    # Target validation.
+    try:
+        if target[:1].isdigit():
+            socket.gethostbyaddr(target)
+        else:
+            socket.gethostbyname(target)
+    except socket.gaierror:
+        console.print(f"[bold red][!][/bold red] Invalid target specified: {target}")
+        sys.exit(1)
 
     received = 0
     miss = 0
+    stop = False
     responses = []
-    
-    # Create ICMP raw socket.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    sock.settimeout(timeout)
-    set_ttl(ttl)
 
-    for n in range(count):
-        try:
-            packet_id = os.getpid() & 0xFFFF
-            header = struct.pack("bbHHh", 8, 0, 0, packet_id, n)
-            data = struct.pack("d", time.time())
-
-            checksum_val = checksum(header + data)
-            header = struct.pack("bbHHh", 8, 0, socket.htons(checksum_val), packet_id, n)
-            packet = header + data
-
-            sock.sendto(packet, (target, 1))
-            start = time.time()
-            reply, addr = sock.recvfrom(1024)
-            duration = (time.time() - start) * 1000
-            responses.append(duration)
-
-            if reply:
-                console.print(f"[bold green][+][/bold green] Ping! - {duration:.2f}ms")
-                received += 1
-        except socket.timeout:
-            console.print("[bold yellow][~][/bold yellow] Request timed out.")        
-            miss += 1
-
-    sock.close()
+    try:
+        ping()
+    except KeyboardInterrupt:
+        stop = True
 
     # Longest, shortest and average response time.
     avg = 0; rmin = float("inf"); rmax = float("-inf")
